15bc8099d118ca3d49b268dfa5125862d910859c
anchul <lacjesusg@gmail.com>
2024-07-24 18:40:51 +0900

    프로젝트 생성

---
+++ src/main/java/org/example/acrcafe/AcrCafeApplication.java
@@ -0,0 +1,13 @@
+package org.example.acrcafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcrCafeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AcrCafeApplication.class, args);
+  }
+
+}
---
+++ src/test/java/org/example/acrcafe/AcrCafeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.acrcafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcrCafeApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
